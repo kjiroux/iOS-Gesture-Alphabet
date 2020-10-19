db2caec298fb5a7fcb8ae21f327f0c1c1862b54b
Kira Jiroux <jirouxk@oregonstate.edu>
2020-10-19 15:54:33 -0700

    This commit is partially a test to see if the branching system through xCode works seamlessly when committing to GH, but also contains code to display accelerometer data to the screen. It has not yet been tested on a physical device.

--- iOS Gesture Alphabet/ViewController.swift
+++ iOS Gesture Alphabet/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var xAccel: UITextField!
+    @IBOutlet weak var yAccel: UITextField!
+    @IBOutlet weak var zAccel: UITextField!
+    
+    var motion = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myAccelerometer()
+    }
+    
+    
+    func myAccelerometer() {
+        motion.accelerometerUpdateInterval = 0.5
+        
+        motion.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            print(data as Any)
+            if let trueData = data {
+                self.view.reloadInputViews()
+                let x = trueData.acceleration.x
+                let y = trueData.acceleration.y
+                let z = trueData.acceleration.z
+            
+                self.xAccel.text = "x: \(Double(x))"
+                self.yAccel.text = "y: \(Double(y))"
+                self.zAccel.text = "z: \(Double(z))"
+            }
+        
+        }
     }
 
 
 }
 
+extension Double {
+    // Rounds the double to decimal place value
+    
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+    
+}
