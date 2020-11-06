f0076d914968693c422b1423c2dadcf7f81fffc9
Kira Jiroux <jirouxk@oregonstate.edu>
2020-11-05 17:21:53 -0800

    Combined Core Motion functions and fixed layout

--- iOS Gesture Alphabet/ViewController.swift
+++ iOS Gesture Alphabet/ViewController.swift
@@ -10,10 +10,23 @@ import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var presenter: UITextField!
     
+    // Accelerometer
     @IBOutlet weak var xAccel: UITextField!
     @IBOutlet weak var yAccel: UITextField!
     @IBOutlet weak var zAccel: UITextField!
+    
+    // Gyroscope
+    @IBOutlet weak var xGyro: UITextField!
+    @IBOutlet weak var yGyro: UITextField!
+    @IBOutlet weak var zGyro: UITextField!
+    
+    // Device Motion
+    @IBOutlet weak var xMotion: UITextField!
+    @IBOutlet weak var yMotion: UITextField!
+    @IBOutlet weak var zMotion: UITextField!
+    
     
     var motion = CMMotionManager()
     
@@ -22,15 +35,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        myAccelerometer()
+        getCoreMotionData()
     }
     
     // This function captures accelerometer data with the CoreMotionManager motion
     // and Updates the UI to display the most recent accelerometer data.
-    func myAccelerometer() 
+    func getCoreMotionData()
     {
         motion.accelerometerUpdateInterval = 0.5
+        motion.gyroUpdateInterval = 0.5
+        motion.deviceMotionUpdateInterval = 0.5
         
+        // Accelerometer
         motion.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
             print(data as Any)
             if let trueData = data {
@@ -45,9 +61,69 @@ class ViewController: UIViewController {
             }
         
         }
+        
+        // Gyroscope
+        motion.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
+            print(data as Any)
+            if let trueData = data {
+                self.view.reloadInputViews()
+                let x = trueData.rotationRate.x
+                let y = trueData.rotationRate.y
+                let z = trueData.rotationRate.z
+                
+                self.xGyro.text = "x: \(Double(x).rounded(toPlaces: 3))"
+                self.yGyro.text = "y: \(Double(y).rounded(toPlaces: 3))"
+                self.zGyro.text = "z: \(Double(z).rounded(toPlaces: 3))"
+                
+            }
+        }
+        
+        // Device Motion; Pitch, Roll, Yaw
+        motion.startDeviceMotionUpdates(to: OperationQueue.current!) { (data, error)
+            in
+            print(data as Any)
+            if let trueData = data {
+                self.view.reloadInputViews()
+                let mPitch = trueData.attitude.pitch
+                let mRoll  = trueData.attitude.roll
+                let mYaw   = trueData.attitude.yaw
+                
+                self.xMotion.text = "Pitch: \(Double(mPitch).rounded(toPlaces: 3))"
+                self.yMotion.text = "Roll: \(Double(mRoll).rounded(toPlaces: 3))"
+                self.zMotion.text = "Yaw: \(Double(mYaw).rounded(toPlaces: 3))"
+            
+                /*
+                 if mRoll < 0.5 && mRoll > -0.5 {
+                    self.presenter.text = ""
+                }
+                 if (mPitch > 1.0) {
+                    if mRoll > 1.3 {
+                        self.presenter.text = "A"
+                        print("A")
+                    }
+                    else if mRoll < -1.2 {
+                        self.presenter.text = "B"
+                        print("B")
+                    }
+                }
+                else if (mPitch > -0.5 && mPitch < 0.5){
+                    if mRoll > 1.3 {
+                        self.presenter.text = "C"
+                        print("C")
+                    }
+                    else if mRoll < -1.2 {
+                        self.presenter.text = "D"
+                        print("D")
+                    }
+                }
+                 */
+                
+            }
+        }
+        
+        
     }
-
-
+    
 }
 
 // Rounds the double to decimal place value
