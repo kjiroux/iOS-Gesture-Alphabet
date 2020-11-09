ff261e284d729504deae62078adc7cf52fa1cba1
morenop127 <pablosmoreno127@gmail.com>
2020-11-08 19:22:05 -0800

    Adds morse code test app

--- iOS Gesture Alphabet/ViewController.swift
+++ iOS Gesture Alphabet/ViewController.swift
@@ -14,8 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var xAccel: UITextField!
     @IBOutlet weak var yAccel: UITextField!
     @IBOutlet weak var zAccel: UITextField!
+    @IBOutlet weak var touchOutput: UITextField!
     
     var motion = CMMotionManager()
+    var morseTimer = 0
     
     override func viewDidLoad() 
     {
@@ -23,7 +25,38 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         myAccelerometer()
+        self.touchOutput.text = "not-touch"
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        morse()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5)
+        {
+            self.makeLong()
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if (morseTimer == 0)
+        {
+            self.touchOutput.text = "short"
+        }
+        else if (morseTimer == 1)
+        {
+            self.touchOutput.text = "long"
+        }
+        else
+        {
+            self.touchOutput.text = "ERROR DETECTING INPUT"
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2)
+        {
+            self.touchOutput.text = ""
+        }
+    }
+    
     
     // This function captures accelerometer data with the CoreMotionManager motion
     // and Updates the UI to display the most recent accelerometer data.
@@ -47,6 +80,17 @@ class ViewController: UIViewController {
         }
     }
 
+    func makeLong()
+    {
+        morseTimer = 1
+    }
+    
+    // This function detects long and short taps
+    func morse()
+    {
+        morseTimer = 0
+        self.touchOutput.text = ""
+    }
 
 }
 
