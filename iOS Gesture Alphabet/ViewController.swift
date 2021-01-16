705807643d241c2fce25742bd8141a48a5bc5ba8
Kira Jiroux <46536292+kjiroux@users.noreply.github.com>
2021-01-15 18:57:22 -0800

    Data record (#8)
    * CoreMotion updates now record "short", "long", and "_" based on device orientation (roll and pitch), which are stored in an array of structs. Struct was made in order to try to save the device orientation data itself, but is proving to be difficult (as when you're trying to record the updated data directly, it makes all entries the last value produced by the updates).
    
    * Correctly receiving Accelerometer data which prints out to the console after each start/stop button press.

--- iOS Gesture Alphabet/ViewController.swift
+++ iOS Gesture Alphabet/ViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 import CoreMotion
 
+struct GestureCapture {
+    //var pitch = ""
+    var rollDir = ""   // roll
+    //var yaw = ""
+}
+
+struct AccelCapture {
+    var xAccel = 0.0
+    var yAccel = 0.0
+    var zAccel = 0.0
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var presenter: UITextField!
@@ -29,24 +41,76 @@ class ViewController: UIViewController {
     
     var motion = CMMotionManager()
     var reset = 0
+    var onSwitch = false
+    var gestureCapture: [GestureCapture] = []
+    var accelCapture: [AccelCapture] = []
+    
+    let updateInterval = 0.25
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+
         // Fixes issue where dark mode causes text to be invisible. PLEASE INCLUDE ON EVERY PAGE
         overrideUserInterfaceStyle = .light
-        getCoreMotionData()
+
     }
 
+    // This is in direct relation to the button and when it is hit. onSwitch is
+    // a boolean variable which determines if CoreMotion data should be started,
+    // as well as clears the gestureCapture array before getting started.
+    @IBAction func recordButton(_ sender: UIButton) {
+        
+        onSwitch = !onSwitch
+        
+        if (onSwitch == true) {
+            print("Switch On.")
+            accelCapture = []
+            gestureCapture = []
+            getCoreMotionData()
+        }
+        else {
+            switchOff()
+        }
+    }
+    
+    
+    // This function turns off all CoreMotion updates after the button has been
+    // hit again, and currently prints out the corresponding gesture information
+    // that has been captured.
+    func switchOff() {
+        print("")
+        print("Switch off.")
+        
+        onSwitch = false
+        motion.stopAccelerometerUpdates()
+        motion.stopGyroUpdates()
+        motion.stopDeviceMotionUpdates()
+        
+        print("_____________________________________________")
+        print("Printing Results:")
+
+        /*
+        for gesture in gestureCapture {
+            print(gesture.rollDir)
+        }
+ */
+        for value in accelCapture {
+            print("X: \(value.xAccel) | Y: \(value.yAccel) | Z: \(value.zAccel)")
+        }
+        
+    }
+    
+    
     // This function captures accelerometer data with the CoreMotionManager motion
     // and Updates the UI to display the most recent accelerometer data.
     func getCoreMotionData()
     {
-        motion.accelerometerUpdateInterval = 0.5
-        motion.gyroUpdateInterval = 0.5
-        motion.deviceMotionUpdateInterval = 0.5
+        motion.accelerometerUpdateInterval = updateInterval
+        motion.gyroUpdateInterval = updateInterval
+        motion.deviceMotionUpdateInterval = updateInterval
         
         // Accelerometer
         motion.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
@@ -60,6 +124,9 @@ class ViewController: UIViewController {
                 self.xAccel.text = "x: \(Double(x))"
                 self.yAccel.text = "y: \(Double(y))"
                 self.zAccel.text = "z: \(Double(z))"
+                
+                self.accelCapture.append(AccelCapture(xAccel: x.rounded(toPlaces: 9), yAccel: y.rounded(toPlaces: 9), zAccel: z.rounded(toPlaces: 9)))
+                
             }
         
         }
@@ -94,20 +161,34 @@ class ViewController: UIViewController {
                 self.yMotion.text = "Roll: \(Double(mRoll).rounded(toPlaces: 3))"
                 self.zMotion.text = "Yaw: \(Double(mYaw).rounded(toPlaces: 3))"
                 
+                
+                
+                
                 if (-0.5 < mRoll && 0.5 > mRoll)
                 {
                     self.reset = 0
+                    
+                    //self.gestureCapture.append(GestureCapture(dir: "_"))
                 }
                 
                 if (self.reset == 0 && -0.5 > mRoll)
                 {
                     self.presenter.text = "short"
                     self.reset = 1
+                    self.gestureCapture.append(GestureCapture(rollDir: "short"))
                 }
                 else if (self.reset == 0 && 0.5 < mRoll)
                 {
                     self.presenter.text = "long"
                     self.reset = 1
+                    
+                    self.gestureCapture.append(GestureCapture(rollDir: "long"))
+                    
+                }
+                else if (self.reset == 0 && 1.0 < mPitch) {
+                    self.presenter.text = "_"
+                    self.reset = 1
+                    self.gestureCapture.append(GestureCapture(rollDir: "_"))
                 }
             
                 /*
