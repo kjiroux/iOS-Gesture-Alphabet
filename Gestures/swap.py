d35ee446abaec1b8bdaf8d87e48e2ae5e40f6452
Pablo Moreno <pablosmoreno127@gmail.com>
2021-02-09 19:28:46 -0800

    Add documentation for swap.py

--- Gestures/swap.py
+++ Gestures/swap.py
@@ -1,3 +1,18 @@
+'''
+File: swap.py
+Author: Pablo Moreno
+---------------------------------------------------------------------------------------------------
+How the script works: This script is used to move 5 test files to training files. This is because
+when testing our machine learning model we determined that we should have more training files and
+less testing files.
+
+How to call the script:
+python swap.py
+
+Note: Since the files have already been swapped, this script will not likely be very useful, but
+may be used as a model in the future if a reverse swap script is necessary.
+'''
+
 from string import ascii_lowercase
 import os
 
